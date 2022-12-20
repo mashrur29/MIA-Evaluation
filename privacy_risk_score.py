d3b8f721ae015291f48bc741a7b5d34275995961
amit-sarker <amitcsedu99@gmail.com>
2022-12-20 18:24:53 -0500

    Final Version

--- privacy_risk_score.py
+++ privacy_risk_score.py
@@ -1,13 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+ # reference: https://github.com/inspire-group/membership-inference-evaluation/blob/master/privacy_risk_score_utils.py
 
 def distrs_compute(tr_values, te_values, tr_labels, te_labels, num_bins=5, log_bins=False, plot_name=None):
-    ### function to compute and plot the normalized histogram for both training and test values class by class.
-    ### we recommand using the log scale to plot the distribution to get better-behaved distributions.
-
-    # print(set(tr_labels))
-
     num_classes = len(set(tr_labels))
     sqr_num = np.ceil(np.sqrt(num_classes))
     tr_distrs, te_distrs, all_bins = [], [], []
@@ -18,16 +13,12 @@ def distrs_compute(tr_values, te_values, tr_labels, te_labels, num_bins=5, log_b
 
     for i in range(num_classes):
         tr_list, te_list = tr_values[tr_labels == i], te_values[te_labels == i]
-        # print("Training list for label ", i, " = ", tr_list)
         if log_bins:
-            # when using log scale, avoid very small number close to 0
             small_delta = 1e-10
             tr_list[tr_list <= small_delta] = small_delta
             te_list[te_list <= small_delta] = small_delta
-        n1, n2 = np.sum(tr_labels == i), np.sum(te_labels == i)
         all_list = np.concatenate((tr_list, te_list))
         max_v, min_v = np.amax(all_list), np.amin(all_list)
-        # print(max_v, min_v)
         plt.subplot(int(sqr_num), int(sqr_num), i + 1)
         if log_bins:
             bins = np.logspace(np.log10(min_v), np.log10(max_v), num_bins + 1)
@@ -55,17 +46,16 @@ def distrs_compute(tr_values, te_values, tr_labels, te_labels, num_bins=5, log_b
 
 def risk_score_compute(tr_distrs, te_distrs, all_bins, data_values, data_labels):
     def find_index(bins, value):
-        # for given n bins (n+1 list) and one value, return which bin includes the value
         if value >= bins[-1]:
-            return len(bins) - 2  # when value is larger than any bins, we assign the last bin
+            return len(bins) - 2
         if value <= bins[0]:
-            return 0  # when value is smaller than any bins, we assign the first bin
+            return 0
         return np.argwhere(bins <= value)[-1][0]
 
     def score_calculate(tr_distr, te_distr, ind):
         if tr_distr[ind] + te_distr[ind] != 0:
             return tr_distr[ind] / (tr_distr[ind] + te_distr[ind])
-        else:  # when both distributions have 0 probabilities, we find the nearest bin with non-zero probability
+        else:
             for t_n in range(1, len(tr_distr)):
                 t_ind = ind - t_n
                 if t_ind >= 0:
@@ -90,6 +80,5 @@ def calculate_risk_score(tr_values, te_values, tr_labels, te_labels, data_values
                          num_bins=5, log_bins=True):
     tr_distrs, te_distrs, all_bins = distrs_compute(tr_values, te_values, tr_labels, te_labels,
                                                     num_bins=num_bins, log_bins=log_bins)
-    # print(all_bins)
     risk_score = risk_score_compute(tr_distrs, te_distrs, all_bins, data_values, data_labels)
     return risk_score
